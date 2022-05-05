a306b297efd51801dcd22df47674039140ee0bc5
deliteser112 <deliteser112@gmail.com>
2022-05-05 18:41:46 +0900

    email-verify-updated

--- imports/ui/assets/index.js
+++ imports/ui/assets/index.js
@@ -1,4 +1,5 @@
 export { default as PlanFreeIcon } from './icon_plan_free';
 export { default as PlanPremiumIcon } from './icon_plan_premium';
 export { default as PlanStarterIcon } from './icon_plan_starter';
+export { default as SentIcon } from './icon_sent';
 export { default as MaintenanceIllustration } from './illustration_maintenance';
